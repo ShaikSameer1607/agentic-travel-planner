a5e4694ffbb03a7f22e9cc66a826d8d02639d10a
ShaikSameer1605 <shaiksameerbasha072@gmail.com>
2025-12-16 13:13:25 +0530

    feat(pwa): enable basic PWA manifest and service worker registration

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(<App />);
+
+// Register service worker for PWA behavior (works after a production build)
+serviceWorkerRegistration.register();
 
